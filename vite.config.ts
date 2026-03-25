3eb858da3bc3dcdc2e2ef21a23e18ca0ab302138
zhuaizhuai <zhuaizhuaizhang@gmail.com>
2026-03-25 23:24:54 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { vercelPreset } from "@vercel/react-router/vite"; // 1. 导入 Vercel 预设
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter({
+      presets: [vercelPreset()], // 2. 在这里启用它
+    }),
+    tsconfigPaths(),
+  ],
 });
